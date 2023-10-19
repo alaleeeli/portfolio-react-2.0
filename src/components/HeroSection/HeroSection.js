019fbb7e1e5e4000664cddc22bbaf65cda3a1ebd
alaleeeli <144134693+alaleeeli@users.noreply.github.com>
2023-10-19 11:10:51 +0400

    Update HeroSection.js

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,7 +1,7 @@
 export default function HeroSection({ path, title }) {
   return (
     <div className="relative pt-15">
-      <img src={path} alt="Intro" className="grayscale-50 h-1/2"/>
+      <img src={path} alt="Intro" className="grayscale-50 md:h-1/2 h-64"/>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 310" className="absolute bottom-0 w-full">
         <path fill="#E5E5E5" d="M0,96L60,80C120,64,240,32,360,58.7C480,85,600,171,720,213.3C840,256,960,256,1080,234.7C1200,213,1320,171,1380,149.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
       </svg>
